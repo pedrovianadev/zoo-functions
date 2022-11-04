2b61f4b73a46348cb94c40aa4b83d8bfd28a5ce0
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-04 15:42:26 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((people) => people.id === id).responsibleFor[0];
+  const animal = data.species.find((anml) => anml.id === employee)
+    .residents.sort((a, b) => b.age - a.age)[0];
+
+  return [animal.name, animal.sex, animal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
