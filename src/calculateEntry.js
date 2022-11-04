c542b2120c87553269fa36a5f0d78132b0d1a3d1
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-04 15:26:08 -0300

    requisito 8 feito acredito que sem erro de lint G_G

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((crianca) => crianca.age < 18).length;
+  const adult = entrants.filter((adulto) => adulto.age >= 18 && adulto.age < 50).length;
+  const senior = entrants.filter((idoso) => idoso.age >= 50).length;
+
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || !entrants.length) return 0;
+
+  const child = prices.child * countEntrants(entrants).child;
+  const adult = prices.adult * countEntrants(entrants).adult;
+  const senior = prices.senior * countEntrants(entrants).senior;
+  return child + adult + senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
