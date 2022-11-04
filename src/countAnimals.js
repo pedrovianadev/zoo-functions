8c6d533cdba5a0cad7edac0bffe674ced65d6538
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-04 14:55:03 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if ( animal === undefined) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const specie = species.find((specie) => specie.name === animal.specie).residents;
+  if (animal.sex) {
+    return specie.filter((specie) => specie.sex === animal.sex).length;
+  }
+  return specie.length;
 }
 
 module.exports = countAnimals;
