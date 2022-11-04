338d3c5e69d90315b8de4cf5530c19aeba151935
Pedro Viana <pedroviana.developer13@gmail.com>
2022-11-04 14:40:46 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.filter((people) => people.managers.includes(managerId))
+    .map((people) => `${people.firstName} ${people.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
